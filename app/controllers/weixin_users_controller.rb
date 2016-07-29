de483eb564cce0fe6692cbbaa7312e37befdaf45
Perish <golovesx@gmail.com>
2016-07-29 15:53:38 +0800

    发送文字消息

--- app/controllers/weixin_users_controller.rb
+++ app/controllers/weixin_users_controller.rb
@@ -3,10 +3,11 @@ class WeixinUsersController < ApplicationController
 
   def index
   	# 存储
-  	session[:path] = params[:path] if params[:path].present?
+  	session[:back_link] = params[:back_link] if params[:back_link].present?
+  	session[:user_token] = params[:user_token] if params[:user_token].present?
   	wu = WeixinUser.find_by(openid: session[:openid])
   	if wu.present?
-  		# 如果用户存在直接返回
+  		# 如果用户存在直接发送用户信息给接口
   		render json: wu
   	else
   		# 用户去认证
@@ -26,18 +27,33 @@ class WeixinUsersController < ApplicationController
 		  	# 保存或者更新weixin_user
 			weixin_user_token.deal_with_weixin_user
 		end
-		render json: weixin_user_token&.weixin_user
+		if weixin_user_token&.weixin_user
+  			# 如果用户存在直接发送用户信息给接口
+			render json: weixin_user_token&.weixin_user
+  		else
+			session.delete(:user_token)
+  			redirect_to session.delete(:back_link)
+  		end
 	else 
 		# 如果获得的access_token 为空就跳转到默认链接
-		# redirect_to session.delete(:path)
-		render text: ""
+		session.delete(:user_token)
+		redirect_to session.delete(:back_link)
 	end
   end
 
 
   # 发送客服消息
   def create
-  	$client.send_text_custom(to_user, content)
+  	users_arr = params[:to_users]
+  	content = params[:content]
+  	if Domains::DOMAINS.include?(request.host) && users_arr.present? && content.present?
+  		users_arr.each do |openid|
+  			$client.send_text_custom(openid, content)
+  		end
+		render json: {status: "success"}  		
+  	else
+  		render json: {status: "fail"}
+  	end
   end
 
   private
@@ -45,7 +61,8 @@ class WeixinUsersController < ApplicationController
   def is_code?
   	if params[:code].blank?
 		# 如果不存在就返回到默认链接
-		redirect_to session.delete(:path) and return
+		session.delete(:user_token)
+		redirect_to session.delete(:back_link) and return
   	end
   end
 

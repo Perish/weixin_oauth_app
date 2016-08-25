93a57c2487bf47cdede7b6ad4bf9f2a1ec48e1ae
Perish <golovesx@gmail.com>
2016-08-25 17:54:51 +0800

    weixin

--- app/controllers/weixin_users_controller.rb
+++ app/controllers/weixin_users_controller.rb
@@ -9,34 +9,37 @@ class WeixinUsersController < ApplicationController
   end
 
   def code
-    Rails.logger.info "state====----------#{params["state"]}"
-    if params["state"] == "weixin"
-    	# 获得openid和access_token
-    	sns_info = $client.get_oauth_access_token(params[:code])
-      Rails.logger.info "code------weixin-----snsn--------#{sns_info.result.inspect}"
-    	if sns_info.en_msg == "ok" 
-    		# 保存或者更新weixin_user_token 如果用户存在直接发送用户信息给接口
-    		token = WeixinUserToken.deal_with_self(sns_info.result)
-        @wu = token&.weixin_user
-        # token&.weixin_user&.post_info(session[:user_token])
-        session[:openid] = sns_info.result["openid"] if sns_info.result["openid"]
-    	end
-    end
-
-    if params["state"] =~ /\d/
-      apid, wuid = params["state"].split("A")
-      client = case apid
-                when "1" then $client1
-                when "2" then $client2
-                end
-      @wu = WeixinUser.find_by(id: wuid)
-      sns_info = client.get_oauth_access_token(params[:code])
-      Rails.logger.info "code------stateid-----snsn--------#{sns_info.result.inspect}"
-      if sns_info.en_msg == "ok" && @wu.present?
-        @wu.weixin_openids.create(openid: sns_info.result["openid"], apid: apid)
+    begin 
+      if params["state"] == "weixin"
+      	# 获得openid和access_token
+      	sns_info = $client.get_oauth_access_token(params[:code])
+        Rails.logger.info "code------weixin-----snsn--------#{sns_info.result.inspect}"
+      	if sns_info.en_msg == "ok" 
+      		# 保存或者更新weixin_user_token 如果用户存在直接发送用户信息给接口
+      		token = WeixinUserToken.deal_with_self(sns_info.result)
+          @wu = token&.weixin_user
+          # token&.weixin_user&.post_info(session[:user_token])
+          session[:openid] = sns_info.result["openid"] if sns_info.result["openid"]
+      	end
       end
+
+      if params["state"] =~ /\d/
+        apid, wuid = params["state"].split("A")
+        client = case apid
+                  when "1" then $client1
+                  when "2" then $client2
+                  end
+        @wu = WeixinUser.find_by(id: wuid)
+        sns_info = client.get_oauth_access_token(params[:code])
+        Rails.logger.info "code------stateid-----snsn--------#{sns_info.result.inspect}"
+        if sns_info.en_msg == "ok" && @wu.present?
+          @wu.weixin_openids.create(openid: sns_info.result["openid"], apid: apid)
+        end
+      end
+      next_redirect(@wu)
+    rescue Exception => e
+      redirect_to session.delete(:back_link)
     end
-    next_redirect(@wu)
   end
 
   # 发送客服消息
@@ -76,11 +79,14 @@ class WeixinUsersController < ApplicationController
   end
 
   def redirect_to_authorize_url(client, scope = "snsapi_userinfo", state = "weixin")
-    redirect_to client.authorize_url(code_weixin_users_url, scope, state) and return
+    begin
+      redirect_to client.authorize_url(code_weixin_users_url, scope, state) and return
+    rescue Exception => e
+      redirect_to session.delete(:back_link)
+    end
   end
 
   def next_redirect(wu)
-    Rails.logger.info "wu----------#{wu.inspect}"
       openids = wu.weixin_openids.to_a.map(&:apid)
       redirect_to session.delete(:back_link) and return if wu.blank?
       if openids.count > 2
@@ -90,7 +96,6 @@ class WeixinUsersController < ApplicationController
         if openids.include?(1)
           redirect_to_authorize_url($client2, "snsapi_base", "2A#{wu.id}")
         else
-          Rails.logger.info "openids======--------#{openids.inspect}"
           redirect_to_authorize_url($client1, "snsapi_base", "1A#{wu.id}")
         end
       end

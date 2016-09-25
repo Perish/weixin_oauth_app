ef405ede35ac4cf68a2bff7b40413d0a6ba9e114
Perish <golovesx@gmail.com>
2016-09-25 23:38:20 +0800

    sf

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -1,17 +1,18 @@
 class WeixinController < ApplicationController
 	protect_from_forgery except: [:receive, :auth]
 	def receive
-		unsafe_params = params.to_unsafe_h
-		Rails.logger.info "unsafe_params==========#{unsafe_params.inspect}"
-		render text: ""
-	end
-
-	def auth
-		tempArr = [params[:timestamp], params[:nonce], "dslf"].sort.join()
-		if params[:signature] == Digest::SHA1.hexdigest(tempArr)
-			Rails.logger.info "signature"
-			render text: params[:echostr]
+		if request.get?
+			tempArr = [params[:timestamp], params[:nonce], "dslf"].sort.join()
+			if params[:signature] == Digest::SHA1.hexdigest(tempArr)
+				Rails.logger.info "signature"
+				render text: params[:echostr]
+			else
+				render text: ""
+			end
 		else
+			ActionController::Parameters.permit_all_parameters = true
+			unsafe_params = params.to_unsafe_h
+			Rails.logger.info "unsafe_params==========#{unsafe_params.inspect}"
 			render text: ""
 		end
 	end
--- app/models/weixin_user.rb
+++ app/models/weixin_user.rb
@@ -31,12 +31,12 @@ class WeixinUser < ApplicationRecord
 			           {	
 			               "type":"view",
 			               "name":"协会简介",
-			               "url":"http://mp.weixin.qq.com/s?__biz=MzAwNzQxNjg1NQ==&tempkey=ttTYmlE2QzEDwKtiae52egbwX7e2nu%2FhXYn9TWkvlr20qdtsnz%2BwehezG1RKgQqDJbtRz%2Fh3axNOxt4ai4C7zGtaz%2FhqD7Y8H87gcbXX4NgHyFCFbDUibPnNlrZDxQAcyKDjoRNRlscXjglvhBTPcw%3D%3D&#rd"
+			               "url":"http://955.cc/jeGub"
 			            },
 			            {
 			               "type":"view",
 			               "name":"评价办法",
-			               "url":"http://mp.weixin.qq.com/s?__biz=MzAwNzQxNjg1NQ==&tempkey=ttTYmlE2QzEDwKtiae52egbwX7e2nu%2FhXYn9TWkvlr0TpBOuCofzSw%2FVMdcQVB%2Fsget2rln%2FE7aXNg0hkZsmJBhvDQ1u13MBWs4wC%2BHW8acHyFCFbDUibPnNlrZDxQAcR1ihSniEw3Mc9%2BSlWYiFzg%3D%3D&#rd"
+			               "url":"http://955.cc/jeHek"
 			            },
 			            {
 			               "type":"view",
